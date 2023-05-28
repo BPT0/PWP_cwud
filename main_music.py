44146b7b8dc881e800385b5eac3e16747b882bd2
komilkiway <81085595+komilkiway@users.noreply.github.com>
2023-05-28 12:17:23 +0900

    입력창 뷰 및 명령어 작성

--- main_music.py
+++ main_music.py
@@ -13,32 +13,42 @@ openai.api_key = "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"
 def index():
     musics = glob.glob("*.xlsx");     # 음악들 데이터로 초기화
     print(musics)   
-    return render_template("listquiz.html",  musics = musics)
+    return render_template("listmusic.html",  musics = musics)
 
 
 @app.route('/make')
-def quiz() :
+def musicList() :
     role = request.args.get("role", "")
-    sel = request.
-    subject = request.args.get("subject", "")
+    
+    mon = request.args.get("mon", "")
+    tue = request.args.get("tue", "")
+    wes = request.args.get("wes", "")
+    thu = request.args.get("thu", "")
+    fri = request.args.get("fri", "")
+    sat = request.args.get("sat", "")
+    sun = request.args.get("sun", "")
+
+    jenre = request.args.get("jenre", "")
+    feel = request.args.get("feel", "")
+    weather = request.args.get("weather", "")
+    
     num = int(request.args.get("num", 5))
+
+    playlistName = request.args.get("playlistName", "")
+    
     prompt  = f"""
     당신은 {role} DJ 입니다.
-    월요일엔 {mon}, 화요일엔 {tue}, 수요일엔 {wes}, 목요일엔 {thu}, 
+    이번주 월요일엔 {mon}, 화요일엔 {tue}, 수요일엔 {wes}, 목요일엔 {thu}, 
     금요일엔 {fri}, 토요일엔 {sat}, 일요일엔, {sun} 음악을 들은 사람에게
-    {subject} 주제를 가진 음악을 추천해주고 음악마다 유튜브 링크를 표시하려 합니다
+    {jenre}이고 {feel}감정일때 {weather}에 적절한 음악을 들으려고 합니다.
+    추천하는 음악과 유튜브 링크를 표시하려 합니다
     please generate python code to write excel file using dataframe in table format with 7 columns.
-    please save the excel file as {subject}.xlsx.
+    please save the excel file as {playlistName}.xlsx.
     python 코드만 작성하고 설명이나 주석없이 코드만 알려주세요
-    
     
     column 1 : 월, column 2 : 화, column 3 : 수, column 4 : 목,  column 5 : 금,  column 6 : 토,  column 7 : 일,
     """
     
-    if(sel == subject){
-        
-    }
-
     print(prompt)
     messages = []
     messages.append({"role": "user", "content": prompt})
@@ -48,27 +58,11 @@ def quiz() :
     exec(res)    
     return  "code : <br/>" + res.replace("\n", "<br/>").replace(" "," &nbsp;")
 
-@app.route('/readquiz/<file>')
-def readquiz(file) :
+
+@app.route('/readmusic/<file>')
+def readmusic(file) :
     df = pd.read_excel(file)     
-    return render_template("readquiz.html", data=df.to_dict('record'))
+    return render_template("readmusic.html", data=df.to_dict('record'))
    
 if __name__ == '__main__':
 	app.run(debug=True)
-
-
-<div class="form-group col-sm-5">
-        <label for="addressKindU">도/특별시 선택</label>
-        <select class="form-control" id="addressKindU" name="addressKindU" onchange="addressKindChange(this)">
-        <option>주소를 선택하세요</option>
-        <option value="a">서울</option>
-        <option value="b">경기도</option>
-        <option value="c">충청남도</option>
-        </select>
-    </div>
-    <div class="form-group col-sm-4">
-        <label for="addressKindD">주소 소분류</label>
-        <select class="form-control" id="addressKindD" name="addressKindD">
-        <option>선택해주세요.</option>
-        </select>
-    </div>
